f9aec34bd8bcbf6007ba3652933cde493c29cf86
mail@matthiasbock.net <mail@matthiasbock.net@1ef8ace3-5f23-5c32-7714-f2e1ca636e4c>
2012-06-17 11:25:23 +0000

    aqhbci-tool

--- trunk/aqbanking.py
+++ trunk/aqbanking.py
@@ -3,12 +3,29 @@
 
 from subprocess import Popen, PIPE
 from shlex import split
+
+import aqhbci_tool4
 from parser import HBCI_response
 
 class AqBanking:
-	
-command = "aqbanking-cli -P input -n request --account=... --transactions"
-print command
+	def __init__(self, url, cert, customerid, accountnumber, bankcode, pin)
+		self.url = url
+		self.cert = cert
+		self.userid = userid
+		self.accountnumber = accountnumber
+		self.bankcode = bankcode
+		self.pin = pin
 
-print HBCI_response( Popen(split(command), stdout=PIPE).communicate()[0] )
+		installed = aqhbci_tool4.listmedia
+		if (not userid in installed.keys()) or (not accountnumber in installed[userid]):
+			m_id = aqhbci_tool.addmedium('pintan')
+			aqhbci_tool.adduser(m_id, url, cert, bankcode, customerid, accountnumber)
+
+	def listaccs(self):
+		return []
+
+	def request_transactions(self):
+		command = '/usr/bin/aqbanking-cli -P /dev/stdin -n request --transactions --account='+self.accountnumber+' --bank='+self.bankcode+'
+		print command
+		return HBCI_response( Popen(split(command), stdout=PIPE).communicate()[0] )
 
---
+++ trunk/aqhbci_tool.py
@@ -0,0 +1,22 @@
+# -*- coding: iso-8859-15 -*-
+#!/usr/bin/python
+
+from subprocess import Popen, PIPE
+from shlex import split
+from parser import HBCI_response
+
+def addmedium(_type):
+	if _type == 'pintan':
+		...
+
+def listmedia():
+aqhbci-tool listmedia
+  Medium 0: "PINTAN-20060823-162456" (pintan)
+	return []
+
+def adduser(medium, url, bankcode, userid, customid):
+	return
+
+def listaccounts():
+	return []
+
